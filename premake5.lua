08c8212c81381ad63ae7dac2604966405a361ad2
Grant Joseph <grantj150@yahoo.com>
2020-10-15 17:48:24 -0400

    add settings for LinuxARM platfor. Add settings for Debug configuration. Add settings for Release configuration

--- premake5.lua
+++ premake5.lua
@@ -8,3 +8,15 @@ workspace("GJGO")
         language("C++")
 
         files({"premake5.lua", ".gitignore", "src/*.cpp", "include/*"})
+
+        filter("platforms:LinuxARM")
+            architecture("ARM")
+            system("linux")
+
+        filter("configurations:Debug")
+            optimize("Off")
+            flags("Symbols")
+
+        filter("configurations:Release")
+            optimize("On")
+            flags({"LinkTimeOptimization"})
