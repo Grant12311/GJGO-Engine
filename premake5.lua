a78869a602f796e27c8e007143c7e63e709b97b4
Grant Joseph <grantj150@yahoo.com>
2020-10-19 19:21:07 -0400

    rewrite much of premake5.lua. Add Druid as a project

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,51 @@
 workspace("GJGO")
+    language("C++")
     toolset("gcc")
     configurations({"Debug", "Release"})
     platforms({"LinuxARM"})
 
+    links({"X11", "GL"})
+
+    buildoptions({"-std=c++1z", "-Wall", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code"})
+
+    filter("platforms:LinuxARM")
+        architecture("ARM")
+        system("linux")
+
+        libdirs({"/usr/lib"})
+
+    filter("configurations:Debug")
+        symbols("On")
+        buildoptions({{"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"}})
+        linkoptions({"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"})
+
+    filter("configurations:Release")
+        optimize("On")
+        flags({"LinkTimeOptimization"})
+
+    project("Druid")
+        filename("Druid")
+        kind("StaticLib")
+
+        files({"vendor/Druid/src/*", "vendor/Druid/include/*"})
+
     project("GJGO")
         filename("GJGO")
         kind("StaticLib")
-        language("C++")
+        links({"Druid"})
 
-        buildoptions({"-std=c++1z", "-Wall", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code"})
         files({"premake5.lua", ".gitignore", "src/*", "include/*"})
 
-        filter("platforms:LinuxARM")
-            architecture("ARM")
-            system("linux")
+    project("Game")
+        filename("Game")
+
+        links({"GJGO", "Druid"})
+        files({"game/src/*"})
 
         filter("configurations:Debug")
-            symbols("On")
-            buildoptions({"-pg"})
-            linkoptions({"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"})
+            kind("ConsoleApp")
 
         filter("configurations:Release")
-            optimize("On")
-            flags({"LinkTimeOptimization"})
+            kind("WindowedApp")
+
+
