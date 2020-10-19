e6d6ad26218220c9b0b199f72cffdf86d03717fa
Grant Joseph <grantj150@yahoo.com>
2020-10-19 19:22:18 -0400

    initial app class definition

---
+++ src/app.cpp
@@ -0,0 +1,19 @@
+#include "../include/app.hpp"
+
+namespace GJGO
+{
+    void Application::run()
+    {
+        while (this->window.isOpen)
+        {
+            this->window.update();
+            //this->window.clear();
+            this->window.swapBuffers();
+        }
+    }
+
+    Application::Application()
+    {
+
+    }
+}
