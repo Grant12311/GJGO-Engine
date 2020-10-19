4b58f0de521817ad2741d6749165f3f851a67511
Grant Joseph <grantj150@yahoo.com>
2020-10-19 19:21:46 -0400

    initial app class declaration

---
+++ include/app.hpp
@@ -0,0 +1,14 @@
+#include <Hangar2/hangar.h>
+
+namespace GJGO
+{
+    class Application
+    {
+    public:
+        Hangar::Window window;
+
+        void run();
+
+        Application();
+    };
+}
