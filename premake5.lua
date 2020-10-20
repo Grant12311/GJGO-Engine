7e6dda084f401cd1daa390ffcfcd6cecf5ba4b56
Grant Joseph <grantj150@yahoo.com>
2020-10-19 22:33:56 -0400

    add build target defines. Include Druid's files recursivly. Add vendor/Druid/include as include dir

--- premake5.lua
+++ premake5.lua
@@ -4,6 +4,7 @@ workspace("GJGO")
     configurations({"Debug", "Release"})
     platforms({"LinuxARM"})
 
+    includedirs({"include", "vendor/Hangar2/include", "vendor/Druid/include"})
     links({"X11", "GL"})
 
     buildoptions({"-std=c++1z", "-Wall", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code"})
@@ -18,10 +19,12 @@ workspace("GJGO")
         symbols("On")
         buildoptions({{"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"}})
         linkoptions({"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"})
+        defines({"GJGO_BUILD_TARGET_DEBUG"})
 
     filter("configurations:Release")
         optimize("On")
         flags({"LinkTimeOptimization"})
+        defines({"GJGO_BUILD_TARGET_RELEASE"})
 
     project("Druid")
         filename("Druid")
@@ -29,7 +32,7 @@ workspace("GJGO")
         location("vendor/Druid")
         targetdir("vendor/Druid/bin/%{cfg.platform}/%{cfg.buildcfg}")
 
-        files({"vendor/Druid/src/*", "vendor/Druid/include/*"})
+        files({"vendor/Druid/src/**", "vendor/Druid/include/**"})
 
     project("GJGO")
         filename("GJGO")
@@ -43,7 +46,6 @@ workspace("GJGO")
         location("examples/Game")
         targetdir("examples/Game/bin/%{cfg.platform}/%{cfg.buildcfg}")
 
-        includedirs({"include"})
         links({"GJGO", "Druid"})
         files({"examples/Game/src/*"})
 
