88e43c2a40ed004983e6a4690f2a874e3b45bbca
Grant Joseph <grantj150@yahoo.com>
2020-10-21 02:00:51 -0400

    basic logging example

--- examples/Game/src/main.cpp
+++ examples/Game/src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-#include "GJGO/app.hpp"
+#include <GJGO/app.hpp>
+#include <GJGO/log.hpp>
 
 int main()
 {
+    GJGO_SET_LOG_LEVEL_WARN();
     GJGO::Application app;
     app.run();
 
