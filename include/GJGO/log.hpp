83d9d6636efa29df02da4e1ce97c20afed046095
Grant Joseph <grantj150@yahoo.com>
2020-10-23 18:31:32 -0400

    make macros variadic

--- include/GJGO/log.hpp
+++ include/GJGO/log.hpp
@@ -5,18 +5,18 @@
 #include <memory>
 
 #ifndef GJGO_BUILD_TARGET_DIST
-    #define GJGO_LOG_INFO(x) GJGO::Log::logInfo(x)
-    #define GJGO_LOG_WARN(x) GJGO::Log::logWarn(x)
-    #define GJGO_LOG_ERROR(x) GJGO::Log::logError(x)
+    #define GJGO_LOG_INFO(x...) GJGO::Log::logInfo(x)
+    #define GJGO_LOG_WARN(x...) GJGO::Log::logWarn(x)
+    #define GJGO_LOG_ERROR(x...) GJGO::Log::logError(x)
 
     #define GJGO_SET_LOG_LEVEL_INFO() GJGO::Log::level = GJGO::Log::Level::INFO
     #define GJGO_SET_LOG_LEVEL_WARN() GJGO::Log::level = GJGO::Log::Level::WARN
     #define GJGO_SET_LOG_LEVEL_ERROR() GJGO::Log::level = GJGO::Log::Level::ERROR
     #define GJGO_SET_LOG_LEVEL_NONE() GJGO::Log::level = GJGO::Log::Level::NONE
 #else
-    #define GJGO_LOG_INFO(x)
-    #define GJGO_LOG_WARN(x)
-    #define GJGO_LOG_ERROR(x)
+    #define GJGO_LOG_INFO(x...)
+    #define GJGO_LOG_WARN(x...)
+    #define GJGO_LOG_ERROR(x...)
 
     #define GJGO_SET_LOG_LEVEL_INFO()
     #define GJGO_SET_LOG_LEVEL_WARN()
