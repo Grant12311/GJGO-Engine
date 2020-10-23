f703ec3c984ab357ca4eb98cb33ff5e822416599
Grant Joseph <grantj150@yahoo.com>
2020-10-23 18:32:11 -0400

    add onUpdateEvent example

--- examples/Game/src/main.cpp
+++ examples/Game/src/main.cpp
@@ -2,9 +2,15 @@
 
 #include <GJGO/app.hpp>
 
+void onUpdate(GJGO::Application* const a_app)
+{
+    a_app->window.setTitle(a_app->window.deltaTime);
+}
+
 int main()
 {
     GJGO::Application app;
+    app.onUpdateEvent.addListener(onUpdate);
     app.run();
 
     return 0;
