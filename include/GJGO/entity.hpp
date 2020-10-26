166837641ceed7151e8a08b0c938f96ad1179394
Grant Joseph <grantj150@yahoo.com>
2020-10-26 16:30:37 -0400

    add first Entity draft

--- include/GJGO/entity.hpp
+++ include/GJGO/entity.hpp
@@ -1,6 +1,8 @@
 #ifndef GJGO_ENTITY_H
 #define GJGO_ENTITY_H
 
+#include <entt/entity/registry.hpp>
+
 #include <GJGO/app.hpp>
 
 namespace GJGO
@@ -8,8 +10,29 @@ namespace GJGO
     class Entity
     {
     public:
-        Entity(GJGO::Application* const a_app);
+        Entity(GJGO::Application* const a_appPtr);
         ~Entity();
+
+        template<typename T, typename... ARGS>
+        void addComponent(ARGS... a_args)
+        {
+            this->m_registryPtr->emplace<T>(this->m_entity, a_args...);
+        }
+
+        template<typename T>
+        const T& getComponent()
+        {
+            return this->m_registryPtr->get<T>(this->m_entity);
+        }
+
+        template<typename T>
+        T& getComponentAccess()
+        {
+            return this->m_registryPtr->get<T>(this->m_entity);
+        }
+    private:
+        entt::registry* m_registryPtr;
+        entt::entity m_entity;
     };
 }
 
--- src/entity.cpp
+++ src/entity.cpp
@@ -1,14 +1,12 @@
-#include <GJGO/entity.h>
+#include <GJGO/entity.hpp>
 
 namespace GJGO
 {
-    Entity::Entity(GJGO::Application* const a_app)
-    {
-
-    }
+    Entity::Entity(GJGO::Application* const a_appPtr) :
+        m_registryPtr(&a_appPtr->registry), m_entity(m_registryPtr->create()) {}
 
     Entity::~Entity()
     {
-
+        this->m_registryPtr->destroy(this->m_entity);
     }
 }
