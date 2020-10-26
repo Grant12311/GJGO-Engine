1fa69d8553f128adc3e3dcf0edf356d8889167ca
Grant Joseph <grantj150@yahoo.com>
2020-10-25 23:15:33 -0400

    add files for entity class

---
+++ include/GJGO/entity.hpp
@@ -0,0 +1,16 @@
+#ifndef GJGO_ENTITY_H
+#define GJGO_ENTITY_H
+
+#include <GJGO/app.hpp>
+
+namespace GJGO
+{
+    class Entity
+    {
+    public:
+        Entity(GJGO::Application* const a_app);
+        ~Entity();
+    };
+}
+
+#endif
---
+++ src/entity.cpp
@@ -0,0 +1,14 @@
+#include <GJGO/entity.h>
+
+namespace GJGO
+{
+    Entity::Entity(GJGO::Application* const a_app)
+    {
+
+    }
+
+    Entity::~Entity()
+    {
+
+    }
+}
