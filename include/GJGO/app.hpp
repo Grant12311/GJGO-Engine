5f11850af73943a8ad8e5d7d401661bb0cc761b4
Grant Joseph <grantj150@yahoo.com>
2020-10-26 16:29:35 -0400

    include entt/entity/registry.hpp. Add an entt registry to Application class

--- include/GJGO/app.hpp
+++ include/GJGO/app.hpp
@@ -1,6 +1,8 @@
 #ifndef GJGO_APP_H
 #define GJGO_APP_H
 
+#include <entt/entity/registry.hpp>
+
 #include <Hangar2/hangar.h>
 
 #include <Beacon/beacon.h>
@@ -11,6 +13,7 @@ namespace GJGO
     {
     public:
         Hangar::Window window;
+        entt::registry registry;
 
         Beacon::Event<GJGO::Application*> onUpdateEvent;
 
