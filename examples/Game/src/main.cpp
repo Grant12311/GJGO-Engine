546a2298b8bae5261859c1ac6cc23083bf7f2343
Grant Joseph <grantj150@yahoo.com>
2020-10-27 01:52:52 -0400

    start working on basic layer example

--- examples/Game/src/main.cpp
+++ examples/Game/src/main.cpp
@@ -3,16 +3,34 @@
 #include <GJGO/app.hpp>
 #include <GJGO/log.hpp>
 #include <GJGO/entity.hpp>
-
-void onUpdate(GJGO::Application* const a_appPtr)
-{
-    a_appPtr->window.setTitle(a_appPtr->window.deltaTime);
-}
+#include <GJGO/layer.hpp>
+#include <GJGO/event.hpp>
 
 struct Position2D
 {
     int x = 0;
     int y = 0;
+};
+
+class GameLayer : public GJGO::Layer
+{
+public:
+    void onUpdate() override
+    {
+        GJGO_LOG_INFO("Game Layer Updated!");
+    }
+
+    void onEvent(GJGO::Event* const a_event) override
+    {
+        switch (a_event->type)
+        {
+            case GJGO::EventType::keyDown:
+                GJGO_LOG_INFO("Key Down: ", a_event->data.keycode);
+                break;
+            case GJGO::EventType::keyUp:
+                break;
+        }
+    }
 };
 
 int main()
@@ -21,13 +39,14 @@ int main()
     GJGO::Log::printFunction = false;
     GJGO::Application app;
 
+    app.layers.emplace_back(new GameLayer);
+
     GJGO::Entity e(&app);
     e.addComponent<Position2D>(5, 5);
     e.editComponent<Position2D>(0, 90);
 
     GJGO_LOG_INFO("(", e.getComponent<Position2D>().x, ", ", e.getComponent<Position2D>().y, ")");
 
-    app.onUpdateEvent.addListener(onUpdate);
     app.run();
 
     return 0;
