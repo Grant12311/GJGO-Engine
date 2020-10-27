641c55f28f33dde90294ce0b1b264229d8f405cc
Grant Joseph <grantj150@yahoo.com>
2020-10-27 01:48:08 -0400

    include GJGO/event.hpp. Add onUpdate and onEvent virtual functions

--- include/GJGO/layer.hpp
+++ include/GJGO/layer.hpp
@@ -1,12 +1,15 @@
 #ifndef GJGO_LAYER_H
 #define GJGO_LAYER_H
 
+#include <GJGO/event.hpp>
+
 namespace GJGO
 {
     class Layer
     {
     public:
-
+        virtual void onUpdate() {}
+        virtual void onEvent(Event* const a_event)  {}
     };
 }
 
