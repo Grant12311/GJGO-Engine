bb8ca798bfbab08e1cfbfed70b2ed938fc0fc788
Grant Joseph <grantj150@yahoo.com>
2020-10-29 19:20:15 -0400

    change type of x and y members from int to int32_t. Include cstdint

--- include/GJGO/position2D.hpp
+++ include/GJGO/position2D.hpp
@@ -1,12 +1,14 @@
 #ifndef GJGO_POSITION2D_H
 #define GJGO_POSITION2D_H
 
+#include <cstdint>
+
 namespace GJGO
 {
     struct Position2D
     {
-        int x = 0;
-        int y = 0;
+        int32_t x = 0;
+        int32_t y = 0;
     };
 }
 
