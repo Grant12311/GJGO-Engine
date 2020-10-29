1afbe0b5fddf39ce6f1bea8a11a7ff08df27204c
Grant Joseph <grantj150@yahoo.com>
2020-10-29 19:26:52 -0400

    add Size2D struct

---
+++ include/GJGO/size2D.hpp
@@ -0,0 +1,15 @@
+#ifndef GJGO_SIZE2D_H
+#define GJGO_SIZE2D_H
+
+#include <cstdint>
+
+namespace GJGO
+{
+    struct Size2D
+    {
+        uint32_t width;
+        uint32_t height;
+    };
+}
+
+#endif
