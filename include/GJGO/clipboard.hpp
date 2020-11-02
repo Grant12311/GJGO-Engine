14e1c0a8b22a635490e5313dc85bb235994fd0a1
Grant Joseph <grantj150@yahoo.com>
2020-11-02 18:01:52 -0500

    add files for clipboard system

---
+++ include/GJGO/clipboard.hpp
@@ -0,0 +1,20 @@
+#ifndef GJGO_CLIPBOARD_H
+#define GJGO_CLIPBOARD_H
+
+namespace GJGO
+{
+    namespace Clipboard
+    {
+        void read()
+        {
+
+        }
+
+        void write()
+        {
+
+        }
+    }
+}
+
+#endif
