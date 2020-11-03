f35a44ab6308c2201450a4408a1214e9dd3eb782
Grant Joseph <grantj150@yahoo.com>
2020-11-03 16:08:09 -0500

    add drawGui virtual function

--- include/GJGO/layer.hpp
+++ include/GJGO/layer.hpp
@@ -13,6 +13,7 @@ namespace GJGO
         virtual void onUpdate() {}
         virtual void onEvent(Event* const /*a_event*/)  {}
         virtual void draw() {}
+        virtual void drawGui() {}
 
         virtual ~Layer() = default;
     };
