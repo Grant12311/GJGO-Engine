e5303931faaf7f01ab167c6b5b9889b9fa6076da
Grant Joseph <grantj150@yahoo.com>
2020-11-07 13:07:35 -0500

    declare Renderer::drawQuad

--- include/GJGO/2D/renderer2D.hpp
+++ include/GJGO/2D/renderer2D.hpp
@@ -1,9 +1,15 @@
 #ifndef GJGO_RENDERER2D_H
 #define GJGO_RENDERER2D_H
 
+#include <GJGO/2D/transform2D.hpp>
+#include <GJGO/color.hpp>
+
 namespace GJGO
 {
-
+    namespace Renderer
+    {
+        void drawQuad(const Position2D a_position, const Size2D a_size, const Color3 a_color = {1.0f, 1.0f, 1.0f});
+    }
 }
 
 #endif
