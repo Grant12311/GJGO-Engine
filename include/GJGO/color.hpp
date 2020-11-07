fee06666067de79073d7e5e043f2cea0c595c050
Grant Joseph <grantj150@yahoo.com>
2020-11-07 13:06:36 -0500

    add color.hpp and Color3 and 4 structs

---
+++ include/GJGO/color.hpp
@@ -0,0 +1,17 @@
+#ifndef GJGO_COLOR3_H
+#define GJGO_COLOR3_H
+
+namespace GJGO
+{
+    struct Color3
+    {
+        float red, green, blue;
+    };
+
+    struct Color4
+    {
+        float red, greem, blue, alpha;
+    };
+}
+
+#endif
