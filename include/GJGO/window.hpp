3b624218e011961626a395f23e70531a5454a6a6
Grant Joseph <grantj150@yahoo.com>
2020-11-09 18:14:13 -0500

    implement window get height and width abstractions

---
+++ include/GJGO/window.hpp
@@ -0,0 +1,13 @@
+#ifndef GJGO_WINDOW_H
+#define GJGO_WINDOW_H
+
+namespace GJGO
+{
+    namespace Window
+    {
+        unsigned int getWidth();
+        unsigned int getHeight();
+    }
+}
+
+#endif
---
+++ src/window.cpp
@@ -0,0 +1,24 @@
+#include <GLFW/glfw3.h>
+
+#include <GJGO/app.hpp>
+
+namespace GJGO
+{
+    unsigned int getWidth()
+    {
+        int width;
+        int height;
+        glfwGetWindowSize(g_appInstancePtr->windowPtr, &width, &height);
+
+        return static_cast<unsigned int>(width);
+    }
+
+    unsigned int getHeight()
+    {
+        int width;
+        int height;
+        glfwGetWindowSize(g_appInstancePtr->windowPtr, &width, &height);
+
+        return height;
+    }
+}
