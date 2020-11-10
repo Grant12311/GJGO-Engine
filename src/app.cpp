86954fa3074f780e2b736428801d14d9dac14c2d
Grant Joseph <grantj150@yahoo.com>
2020-11-10 00:16:57 -0500

    remove paramter names in OpenGL error callback function to prevent warnings

--- src/app.cpp
+++ src/app.cpp
@@ -12,7 +12,7 @@
 
 namespace GJGO
 {
-    static void openglDebugLogger(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+    static void openglDebugLogger(GLenum /*source*/, GLenum /*type*/, GLuint /*id*/, GLenum severity, GLsizei /*length*/, const GLchar* message, const void* /*userParam*/)
     {
         switch (severity)
         {
