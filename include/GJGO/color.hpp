1233f125bdd5c26025368b5fe9bf5620b8a09958
Grant Joseph <granetejo@gmail.com>
2021-01-01 00:41:06 -0500

    add default values to color structures. Fix namming issue in Color4

--- include/GJGO/color.hpp
+++ include/GJGO/color.hpp
@@ -5,12 +5,17 @@ namespace GJGO
 {
     struct Color3
     {
-        float red, green, blue;
+        float red = 1.0f;
+        float green = 1.0f;
+        float blue = 1.0f;
     };
 
     struct Color4
     {
-        float red, greem, blue, alpha;
+        float red = 1.0f;
+        float green = 1.0f;
+        float blue = 1.0f;
+        float alpha = 1.0f;
     };
 }
 
