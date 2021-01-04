2e164a0e80c853d1b281327fb8f709eaf90c3c91
Grant Joseph <granetejo@gmail.com>
2021-01-03 20:50:07 -0500

    add isDone system to animation system

--- include/GJGO/animation.hpp
+++ include/GJGO/animation.hpp
@@ -9,10 +9,13 @@ namespace GJGO
 {
     class Animation
     {
+    protected:
+        bool m_done = false;
     public:
         bool paused = false;
 
         virtual void step() = 0;
+        virtual bool isDone() = 0;
         virtual ~Animation() = default;
     };
 
@@ -23,6 +26,7 @@ namespace GJGO
         tweeny::tween<int> m_tweenX, m_tweenY;
     public:
         virtual void step() override;
+        virtual bool isDone() override;
         AnimationPosition2D(const unsigned int a_time, Position2D &a_posRef, const Position2D &a_targetPos);
         AnimationPosition2D();
     };
