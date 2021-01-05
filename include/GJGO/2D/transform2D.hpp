81faf2165806e5891555bf4909498749e1f6e2d1
Grant Joseph <granetejo@gmail.com>
2021-01-05 17:30:47 -0500

    add + and - operators and cout support for Position2D

--- include/GJGO/2D/transform2D.hpp
+++ include/GJGO/2D/transform2D.hpp
@@ -9,7 +9,22 @@ namespace GJGO
     {
         int x = 0;
         int y = 0;
+
+        Position2D operator+(const Position2D &a_rhs)
+        {
+            return Position2D{this->x + a_rhs.x, this->y + a_rhs.y};
+        }
+
+        Position2D operator-(const Position2D &a_rhs)
+        {
+            return Position2D{this->x - a_rhs.x, this->y - a_rhs.y};
+        }
     };
+
+    inline std::ostream& operator<<(std::ostream &a_os, const Position2D &a_pos)
+    {
+        return a_os << "(" << a_pos.x << ", " << a_pos.y << ")";
+    }
 
     struct Size2D
     {
