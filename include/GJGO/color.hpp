fb49b48598a81b9687d5e46b773b10370787d385
Grant Joseph <granetejo@gmail.com>
2021-01-13 17:35:08 -0500

    add constructors to Color4

--- include/GJGO/color.hpp
+++ include/GJGO/color.hpp
@@ -1,6 +1,8 @@
 #ifndef GJGO_COLOR3_H
 #define GJGO_COLOR3_H
 
+#include <glm/vec4.hpp>
+
 namespace GJGO
 {
     struct Color3
@@ -16,6 +18,12 @@ namespace GJGO
         float green = 1.0f;
         float blue = 1.0f;
         float alpha = 1.0f;
+
+        Color4(const float a_red = 0.0f, const float a_green = 0.0f, const float a_blue = 0.0f, const float a_alpha = 0.0f) :
+            red(a_red), green(a_green), blue(a_blue), alpha(a_alpha) {}
+
+        Color4(const glm::vec4 &a_vec = {0.0f, 0.0f, 0.0f, 0.0f}) :
+            red(a_vec.x), green(a_vec.y), blue(a_vec.z), alpha(a_vec.w) {}
     };
 }
 
