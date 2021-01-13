22105489fa14811f38c9309edc91b44e422f25b0
Grant Joseph <granetejo@gmail.com>
2021-01-13 17:35:33 -0500

    add constructors to Position2D and Size2D

--- include/GJGO/2D/transform2D.hpp
+++ include/GJGO/2D/transform2D.hpp
@@ -2,13 +2,14 @@
 #define GJGO_TRANSFORM2D_H
 
 #include <glm/mat4x4.hpp>
+#include <glm/vec2.hpp>
 
 namespace GJGO
 {
     struct Position2D
     {
-        int x = 0;
-        int y = 0;
+        int x;
+        int y;
 
         Position2D operator+(const Position2D &a_rhs)
         {
@@ -19,6 +20,12 @@ namespace GJGO
         {
             return Position2D{this->x - a_rhs.x, this->y - a_rhs.y};
         }
+
+        Position2D(const int a_x = 0, const int a_y = 0) :
+            x(a_x), y(a_y) {}
+
+        Position2D(const glm::vec2 &a_vec = {0, 0}) :
+            x(a_vec.x), y(a_vec.y) {}
     };
 
     inline std::ostream& operator<<(std::ostream &a_os, const Position2D &a_pos)
@@ -28,8 +35,14 @@ namespace GJGO
 
     struct Size2D
     {
-        unsigned int width  = 0;
-        unsigned int height = 0;
+        unsigned int width;
+        unsigned int height;
+
+        Size2D(const unsigned int a_width = 0, const unsigned int a_height = 0) :
+            width(a_width), height(a_height) {}
+
+        Size2D(const glm::vec2 &a_vec = {0, 0}) :
+            width(a_vec.x), height(a_vec.y) {}
     };
 
     struct Transform2D
