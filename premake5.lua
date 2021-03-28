e4c0f639041cd88b067da761bcdbb37ba30f5ad5
Grant Joseph <granetejo@gmail.com>
2021-03-27 21:06:28 -0400

    init (new template C++ project)

---
+++ premake5.lua
@@ -0,0 +1,49 @@
+workspace "GJGO"
+    language "C++"
+    cppdialect "C++17"
+    configurations {"Debug", "Release"}
+    platforms {"LinuxX64", "LinuxX86", "LinuxARM", "WindowsARM", "WindowsX86", "WindowsX64"}
+    startproject "GJGO"
+
+    flags {"MultiProcessorCompile"}
+    buildoptions {"-pipe", "-Wall"}
+    defines {}
+
+    links {}
+    includedirs {"%{wks.location}/include/"}
+
+    filter "action:vs*"
+        toolset "msc"
+
+    filter "action:not vs*"
+        toolset "gcc"
+        buildoptions {"-Wextra", "-Wnon-virtual-dtor", "-Winline", "-Wunreachable-code", "-Wshadow", "-Wconversion", "-Wno-switch", "-Wno-unused-variable"}
+
+    filter "platforms:Linux*"
+        system "linux"
+    filter "platforms:Windows*"
+        system "windows"
+
+    filter "platforms:*ARM"
+        architecture "ARM"
+    filter "platforms:*X86"
+        architecture "x86"
+    filter "platforms:*X64"
+        architecture "x86_64"
+
+    filter "configurations:Debug"
+        symbols "On"
+        buildoptions {"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"}
+        linkoptions {"-pg", "-fsanitize=address", "-fsanitize=leak", "-static-libasan"}
+
+    filter "configurations:Release"
+        optimize "On"
+
+    project "GJGO"
+        kind "ConsoleApp"
+        location "%{wks.location}"
+        targetdir "%{prj.location}/bin/%{cfg.platform}/%{cfg.buildcfg}"
+
+        files {"%{prj.location}/src/**.cpp", "%{prj.location}/include/**.hpp", "%{prj.location}/include/**.h", "%{prj.location}/premake5.lua"}
+
+
