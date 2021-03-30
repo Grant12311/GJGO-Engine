1c711b32d044c47065b92ac144e37d966f9822eb
Grant Joseph <granetejo@gmail.com>
2021-03-30 16:36:48 -0400

    Add Layer interface class

---
+++ include/GJGO/layer.hpp
@@ -0,0 +1,24 @@
+#ifndef GJGO_LAYER_H
+#define GJGO_LAYER_H
+
+#include <GJGO/event.hpp>
+
+namespace GJGO
+{
+    class App;
+
+    class Layer
+    {
+    public:
+        const char* name;
+
+        virtual void onUpdate() {}
+        virtual void onEvent(const Event &/*a_event*/)  {}
+        virtual void draw() {}
+        virtual void drawGui() {}
+
+        virtual ~Layer() = default;
+    };
+}
+
+#endif
