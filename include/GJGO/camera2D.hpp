088272cd7f8fe6d5ff2091bfdf9c8157e20b07f9
Grant Joseph <granetejo@gmail.com>
2021-04-06 23:32:51 -0400

    Store a primary camera.

--- include/GJGO/camera2D.hpp
+++ include/GJGO/camera2D.hpp
@@ -1,6 +1,8 @@
 #ifndef GJGO_CAMERA2D_H
 #define GJGO_CAMERA2D_H
 
+#include <memory>
+
 #include <glm/vec3.hpp>
 
 namespace GJGO
@@ -10,6 +12,8 @@ namespace GJGO
     public:
         glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
         float rotation = 0.0f;
+
+        inline static std::unique_ptr<Camera2D> primary = std::make_unique<Camera2D>();
     };
 }
 
