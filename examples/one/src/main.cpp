90fc356de12c42da6601a28d544f1c5e5f737ad5
Grant Joseph <granetejo@gmail.com>
2021-08-16 19:54:49 -0400

    Very basic demo and fade effect.

--- examples/one/src/main.cpp
+++ examples/one/src/main.cpp
@@ -5,32 +5,32 @@
 
 #include <GJGO/gjgo.hpp>
 
+constexpr int GBA_SCREEN_WDITH = 240;
+constexpr int GBA_SCREEN_HEIGHT = 160;
+constexpr int GBA_SCALE = 3;
+
+constexpr int TILE_SIZE = 16  * GBA_SCALE;
+
 class GameLayer final : public GJGO::Layer
 {
 public:
-    GJGO::Entity e1;
-    GJGO::Entity e2;
+    GJGO::Entity fadeScreen;
+    bool doingFadeOut, doingFadeIn;
+
+    GJGO::Entity player;
 
     GameLayer() :
-        Layer("Game Layer"), e1(GJGO::Entity::create()), e2(GJGO::Entity::create())
+        Layer("Game Layer"), fadeScreen(GJGO::Entity::create()), doingFadeOut(false), doingFadeIn(false), player(GJGO::Entity::create())
     {
-        e1.addComponent<GJGO::Transform2DComponent>(glm::vec3(100.0, 100.0, 1.0), glm::vec2(100.0, 100.0));
-        e2.addComponent<GJGO::Transform2DComponent>(glm::vec3(215.0, 215.0, 0.0), glm::vec2(100.0, 100.0));
+        this->fadeScreen.addComponent<GJGO::Transform2DComponent>(glm::vec3(0.0, 0.0, 50.0), glm::vec2(GBA_SCREEN_WDITH * GBA_SCALE, GBA_SCREEN_HEIGHT * GBA_SCALE));
+        this->fadeScreen.addComponent<GJGO::SpriteComponent>(nullptr, glm::vec4(0.0, 0.0, 0.0, 0.0));
 
-        e1.addComponent<GJGO::SpriteComponent>();
-        e2.addComponent<GJGO::SpriteComponent>();
+        this->player.addComponent<GJGO::Transform2DComponent>(glm::vec3(9 * TILE_SIZE, 4 * TILE_SIZE, 1.0), glm::vec2(14 * GBA_SCALE, 21 * GBA_SCALE));
+        this->player.addComponent<GJGO::SpriteComponent>(GJGO::Texture2D::create("res/Player/Male/standing south.png", GJGO::TextureSettings::standard, GL_NEAREST, GL_NEAREST));
 
-        e1.addComponent<GJGO::CollisionBox2DComponent>(std::function<void(GJGO::Entity, GJGO::Entity)>(),
-        [](GJGO::Entity a_e1, GJGO::Entity a_e2) -> void
-        {
-            a_e2.getComponent<GJGO::SpriteComponent>().color = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
-        },
-        [](GJGO::Entity a_e1, GJGO::Entity a_e2) -> void
-        {
-            a_e2.getComponent<GJGO::SpriteComponent>().color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-        });
-
-        e2.addComponent<GJGO::CollisionBox2DComponent>();
+        GJGO::Entity outsideBackgroundEntity = GJGO::Entity::create();
+        outsideBackgroundEntity.addComponent<GJGO::Transform2DComponent>(glm::vec3(0.0, 0.0, 0.0), glm::vec2(383 * GBA_SCALE, 359 * GBA_SCALE));
+        outsideBackgroundEntity.addComponent<GJGO::SpriteComponent>(GJGO::Texture2D::create("res/Backgrounds/Littleroot Town/outside.png", GJGO::TextureSettings::standard, GL_NEAREST, GL_NEAREST));
     }
 
     virtual void onUpdate() override
@@ -55,7 +55,41 @@ public:
             timePassed += GJGO::App::instance->deltaTime;
         }
 
-        if (GJGO::Keyboard::keyIsDown(GLFW_KEY_W))
+        constexpr double WAIT_TIME = 500.0;
+        static double timeWaited = 0.0;
+
+        if (this->doingFadeOut)
+        {
+            GJGO::SpriteComponent& sprite = this->fadeScreen.getComponent<GJGO::SpriteComponent>();
+
+            if (sprite.color.a >= 1.0)
+            {
+                sprite.color.a = 1.0;
+                this->doingFadeOut = false;
+                this->doingFadeIn = true;
+            }else{
+                sprite.color.a += 0.0025 * GJGO::App::instance->deltaTime;
+            }
+        }else if (this->doingFadeIn){
+            if (timeWaited >= WAIT_TIME)
+            {
+                GJGO::SpriteComponent& sprite = this->fadeScreen.getComponent<GJGO::SpriteComponent>();
+
+                if (sprite.color.a <= 0.0)
+                {
+                    sprite.color.a = 0.0;
+                    this->doingFadeOut = false;
+                    this->doingFadeIn = false;
+                    timeWaited = 0.0;
+                }else{
+                    sprite.color.a -= 0.0025 * GJGO::App::instance->deltaTime;
+                }
+            }else{
+                timeWaited += GJGO::App::instance->deltaTime;
+            }
+        }
+
+        /*if (GJGO::Keyboard::keyIsDown(GLFW_KEY_W))
             e1.getComponent<GJGO::Transform2DComponent>().position.y += 1.0 * GJGO::App::instance->deltaTime;
 
         if (GJGO::Keyboard::keyIsDown(GLFW_KEY_S))
@@ -65,7 +99,7 @@ public:
             e1.getComponent<GJGO::Transform2DComponent>().position.x -= 1.0 * GJGO::App::instance->deltaTime;
 
         if (GJGO::Keyboard::keyIsDown(GLFW_KEY_D))
-            e1.getComponent<GJGO::Transform2DComponent>().position.x += 1.0 * GJGO::App::instance->deltaTime;
+            e1.getComponent<GJGO::Transform2DComponent>().position.x += 1.0 * GJGO::App::instance->deltaTime;*/
     }
 
     virtual void onEvent(const GJGO::Event &a_event) override
@@ -80,6 +114,21 @@ public:
                         break;
                     case GLFW_KEY_MINUS:
                         GJGO::Renderer::useBatchRendererAsDefault = false;
+                        break;
+                    case GLFW_KEY_SPACE:
+                        this->doingFadeOut = true;
+                        break;
+                    case GLFW_KEY_W:
+                        this->player.getComponent<GJGO::Transform2DComponent>().position.y += TILE_SIZE;
+                        break;
+                    case GLFW_KEY_A:
+                        this->player.getComponent<GJGO::Transform2DComponent>().position.x -= TILE_SIZE;
+                        break;
+                    case GLFW_KEY_S:
+                        this->player.getComponent<GJGO::Transform2DComponent>().position.y -= TILE_SIZE;
+                        break;
+                    case GLFW_KEY_D:
+                        this->player.getComponent<GJGO::Transform2DComponent>().position.x += TILE_SIZE;
                         break;
                 }
                 break;
@@ -96,7 +145,9 @@ public:
 
 int main()
 {
-    GJGO::App app;
+    GJGO::Renderer::useBatchRendererAsDefault = false;
+
+    GJGO::App app(GJGO::AppSettings("PKMN", GBA_SCREEN_WDITH * GBA_SCALE, GBA_SCREEN_HEIGHT * GBA_SCALE, false));
     app.layers.emplace_back(new GameLayer);
     app.run();
 
